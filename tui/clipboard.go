be0e730e29797770982ae17436295fab87e6a0cf
kota <52403688+yashikota@users.noreply.github.com>
2026-08-25 17:59:47 +0900

    refactor(tui): write clipboard directly (#202)

--- tui/clipboard.go
+++ tui/clipboard.go
@@ -6,16 +6,10 @@ import (
 	"golang.design/x/clipboard"
 )
 
-type clipboardWriteFunc func(context.Context, clipboard.Format, []byte, ...clipboard.Option) (<-chan struct{}, error)
-
 func copyToClipboard(text string) error {
-	return copyToClipboardWith(context.Background(), text, clipboard.Init, clipboard.Write)
-}
-
-func copyToClipboardWith(ctx context.Context, text string, initClipboard func() error, write clipboardWriteFunc) error {
-	if err := initClipboard(); err != nil {
+	if err := clipboard.Init(); err != nil {
 		return err
 	}
-	_, err := write(ctx, clipboard.FmtText, []byte(text))
+	_, err := clipboard.Write(context.Background(), clipboard.FmtText, []byte(text))
 	return err
 }
